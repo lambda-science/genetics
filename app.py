d97a19f22ec538a330970294543810199176e32b
Corentin M <meyer.corentin@gmail.com>
2017-10-07 18:14:38 +0200

    Fasta format support

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class SequenceForm(FlaskForm):
 def index():
 	form = SequenceForm()
 	if form.validate_on_submit():
-		if isSequenceValid(form.sequenceADN.data):
+		if isSequenceValid(fastaConvert(form.sequenceADN.data)[1]):
 			session['SequenceADN'] = form.sequenceADN.data
 			session['simpleBool'] = form.motifsimpleBool.data
 			session['motifSimple'] = form.motifsimple.data
@@ -30,32 +30,37 @@ def index():
 			session['signatureSize'] = form.signature.data
 			return redirect('/resultats')
 		else:
-			flash('SEQUENCE INVALIDE')
+			flash(repr(fastaConvert(form.sequenceADN.data)[1]))
 	else:
 		flash('FORMULAIRE VIDE')
 	return render_template('index.html', title ='Acceuil:', form=form)
 
 @app.route('/resultats')
 def resultat():
-	compo = composition(session['SequenceADN'])
-	gc = int(pourcentGC(session['SequenceADN']))
-	fusion = int(tempFusionHowley(session['SequenceADN']))
-	arn = ADN2ARN(session['SequenceADN'])
+	seqTitle = fastaConvert(session['SequenceADN'])[0]
+	sequence = fastaConvert(session['SequenceADN'])[1]
+	compo = composition(sequence)
+	gc = int(pourcentGC(sequence))
+	fusion = int(tempFusionHowley(sequence))
+	arn = ADN2ARN(sequence)
 	prot = traduction(arn)
 	posMotifSimple = ["N/A"]
 	posMotifComplexe = ["N/A"]
 	sortedSigna = ["N/A"]
 	if session['simpleBool']:
-		posMotifSimple = localiserMotifSimple(session['SequenceADN'],session['motifSimple'], 0)
+		posMotifSimple = localiserMotifSimple(sequence,session['motifSimple'], 0)
 
 	if session['complexeBool']:
-		posMotifComplexe = localiserMotifSimple(session['SequenceADN'],session['motifComplexe'], 0)
+		posMotifComplexe = localiserMotifSimple(sequence,session['motifComplexe'], 0)
 
 	if session['signaBool']:
-		signa = signature(session['SequenceADN'], session['signatureSize'])
+		signa = signature(sequence, session['signatureSize'])
 		sortedSigna = sorted(signa.items(), key=lambda x: x[1], reverse=True)
-	return render_template('resultats.html', title ='Resultats', composition=compo, gc=gc, fusion=fusion, arn=arn, prot=prot,
-	length=len(session['SequenceADN']),posMotifSimple= posMotifSimple, posMotifComplexe=posMotifComplexe, signa=sortedSigna, session=session)
+	return render_template('resultats.html', title ='Resultats', seqTitle = seqTitle, composition=compo, gc=gc, fusion=fusion, arn=arn, prot=prot,
+	length=len(sequence),posMotifSimple= posMotifSimple, posMotifComplexe=posMotifComplexe, signa=sortedSigna, session=session)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	# For server developpement
+	# app.run(debug=False, host="0.0.0.0", port=5010)
+	# For local testing
+	app.run(debug=True, host="127.0.0.1", port=5010)
--- genetics.py
+++ genetics.py
@@ -1,121 +1,142 @@
 import re
 
+def fastaConvert(fastaString):
+    if fastaString[0] == ">":
+        endLineIndex = fastaString.index('\n')
+        title = fastaString[:endLineIndex]
+        sequence = fastaString[endLineIndex+1:]
+        sequence = sequence.replace("\n", "");
+        sequence = sequence.replace("\r", "");
+        return [title, sequence]
+    else:
+        fastaString = fastaString.replace("\n", "");
+        fastaString = fastaString.replace("\r", "");
+        return ["Untitled", fastaString]
+
+
 def isSequenceValid(sequenceADN):
-	if sequenceADN != "":
-		sequenceADN = set(list(sequenceADN.lower()))
-		a = True
-		for i in sequenceADN:
-			if not (i in ['a', 'c', 't', 'g']):
-				a = False
-		return a
-	else:
-		return False
+    if sequenceADN != "":
+            sequenceADN = set(list(sequenceADN.lower()))
+            a = True
+            for i in sequenceADN:
+                if not (i in ['a', 'c', 't', 'g']):
+                    a = False
+            return a
+    else:
+        return False
+
 
 def composition(sequenceADN):
-	composition={}
-	for i in sequenceADN.lower():
-		composition[i] = composition.get(i, 0) + 1
-	return composition
+    composition = {}
+    for i in sequenceADN.lower():
+        composition[i] = composition.get(i, 0) + 1
+    return composition
+
 
 def pourcentGC(sequenceADN):
-	compositionADN = composition(sequenceADN)
-	pourcentGC = 100*(compositionADN["g"] + compositionADN["c"])/(len(sequenceADN))
-	return pourcentGC
+    compositionADN = composition(sequenceADN)
+    pourcentGC = 100 * (compositionADN["g"] + compositionADN["c"]) / (len(sequenceADN))
+    return pourcentGC
+
 
 def tempFusionHowley(sequenceADN):
-	GC = pourcentGC(sequenceADN)
-	Tm = 67.5 + (0.34*GC)-(395/len(sequenceADN))
-	return Tm
+    GC = pourcentGC(sequenceADN)
+    Tm = 67.5 + (0.34*GC)-(395/len(sequenceADN))
+    return Tm
+
 
 def estComplementaire(brinA, brinB):
-	codeComplementaire = {"a":"t", "t":"a", "g":"c", "c":"g"}
-	brinB = brinB[::-1]
-	complementaireBrinB = [ codeComplementaire[i] for i in brinB ]
-	complementaireBrinB = ''.join(complementaireBrinB)
+    codeComplementaire = {"a":"t", "t":"a", "g":"c", "c":"g"}
+    brinB = brinB[::-1]
+    complementaireBrinB = [ codeComplementaire[i] for i in brinB ]
+    complementaireBrinB = ''.join(complementaireBrinB)
 
-	if brinA == complementaireBrinB:
-		return True
-	else:
-		return False
+    if brinA == complementaireBrinB:
+        return True
+    else:
+        return False
+
 
 def ADN2ARN(sequenceADN):
-	sequenceARN = []
-	sequenceADN = sequenceADN.lower()
-	for i in sequenceADN:
-		if i == "t":
-			sequenceARN.append("a")
-		elif i == "a":
-			sequenceARN.append("u")
-		elif i == "c":
-			sequenceARN.append("g")
-		elif i == "g":
-			sequenceARN.append("c")
+    sequenceARN = []
+    sequenceADN = sequenceADN.lower()
+    for i in sequenceADN:
+        if i == "t":
+            sequenceARN.append("a")
+        elif i == "a":
+            sequenceARN.append("u")
+        elif i == "c":
+            sequenceARN.append("g")
+        elif i == "g":
+            sequenceARN.append("c")
 
-	sequenceARN = ''.join(sequenceARN)
-	return sequenceARN
+    sequenceARN = ''.join(sequenceARN)
+    return sequenceARN
 
 
 def traduction(sequenceARN):
-	code_genetique = {
-	'uuu': 'F', 'ucu': 'S', 'uau': 'Y', 'ugu': 'C',
-	'uuc': 'F', 'ucc': 'S', 'uac': 'Y', 'ugc': 'C',
-	'uua': 'L', 'uca': 'S', 'uaa': '*', 'uga': '*',
-	'uug': 'L', 'ucg': 'S', 'uag': '*', 'ugg': 'W',
-	'cuu': 'L', 'ccu': 'P', 'cau': 'H', 'cgu': 'R',
-	'cuc': 'L', 'ccc': 'P', 'cac': 'H', 'cgc': 'R',
-	'cua': 'L', 'cca': 'P', 'caa': 'Q', 'cga': 'R',
-	'cug': 'L', 'ccg': 'P', 'cag': 'Q', 'cgg': 'R',
-	'auu': 'I', 'acu': 'T', 'aau': 'N', 'agu': 'S',
-	'auc': 'I', 'acc': 'T', 'aac': 'N', 'agc': 'S',
-	'aua': 'I', 'aca': 'T', 'aaa': 'K', 'aga': 'R',
-	'aug': 'M', 'acg': 'T', 'aag': 'K', 'agg': 'R',
-	'guu': 'V', 'gcu': 'A', 'gau': 'D', 'ggu': 'G',
-	'guc': 'V', 'gcc': 'A', 'gac': 'D', 'ggc': 'G',
-	'gua': 'V', 'gca': 'A', 'gaa': 'E', 'gga': 'G',
-	'gug': 'V', 'gcg': 'A', 'gag': 'E', 'ggg': 'G',
-	}	
-	sequenceProteineAll = []
-	for cadre in range(0,3):
+    code_genetique = {
+    'uuu': 'F', 'ucu': 'S', 'uau': 'Y', 'ugu': 'C',
+    'uuc': 'F', 'ucc': 'S', 'uac': 'Y', 'ugc': 'C',
+    'uua': 'L', 'uca': 'S', 'uaa': '*', 'uga': '*',
+    'uug': 'L', 'ucg': 'S', 'uag': '*', 'ugg': 'W',
+    'cuu': 'L', 'ccu': 'P', 'cau': 'H', 'cgu': 'R',
+    'cuc': 'L', 'ccc': 'P', 'cac': 'H', 'cgc': 'R',
+    'cua': 'L', 'cca': 'P', 'caa': 'Q', 'cga': 'R',
+    'cug': 'L', 'ccg': 'P', 'cag': 'Q', 'cgg': 'R',
+    'auu': 'I', 'acu': 'T', 'aau': 'N', 'agu': 'S',
+    'auc': 'I', 'acc': 'T', 'aac': 'N', 'agc': 'S',
+    'aua': 'I', 'aca': 'T', 'aaa': 'K', 'aga': 'R',
+    'aug': 'M', 'acg': 'T', 'aag': 'K', 'agg': 'R',
+    'guu': 'V', 'gcu': 'A', 'gau': 'D', 'ggu': 'G',
+    'guc': 'V', 'gcc': 'A', 'gac': 'D', 'ggc': 'G',
+    'gua': 'V', 'gca': 'A', 'gaa': 'E', 'gga': 'G',
+    'gug': 'V', 'gcg': 'A', 'gag': 'E', 'ggg': 'G',
+    }
+    sequenceProteineAll = []
+    for cadre in range(0,3):
 
-		sequenceCodon = [ sequenceARN[j:j+3] for j in range(cadre, len(sequenceARN), 3) ]
-		sequenceProteine = [ code_genetique[i] for i in sequenceCodon if i in code_genetique ]
-		sequenceProteine = '-'.join(sequenceProteine)
-		sequenceProteineAll.append(sequenceProteine)
+        sequenceCodon = [ sequenceARN[j:j+3] for j in range(cadre, len(sequenceARN), 3) ]
+        sequenceProteine = [ code_genetique[i] for i in sequenceCodon if i in code_genetique ]
+        sequenceProteine = '-'.join(sequenceProteine)
+        sequenceProteineAll.append(sequenceProteine)
 
-	return sequenceProteineAll
+    return sequenceProteineAll
 
 
 def localiserMotifSimple(sequenceADN, motif, positionRecherche):
-	motif = motif.lower()
-	sequenceADN = sequenceADN.lower()
-	sequenceADN = sequenceADN[positionRecherche::]
-	pos = 0
-	listePosMotif = []
-	pos = sequenceADN.find(motif)
-	while pos != -1:
-		listePosMotif.append(pos)
-		pos = sequenceADN.find(motif, pos+1)
-	return listePosMotif
+    motif = motif.lower()
+    sequenceADN = sequenceADN.lower()
+    sequenceADN = sequenceADN[positionRecherche::]
+    pos = 0
+    listePosMotif = []
+    pos = sequenceADN.find(motif)
+    while pos != -1:
+        listePosMotif.append(pos)
+        pos = sequenceADN.find(motif, pos+1)
+    return listePosMotif
+
 
 def localiserMotifRE(sequenceADN, motif, positionRecherche):
-	sequenceADN = sequenceADN.upper()
-	pattern = re.compile(motif)
-	listPosMotif = []
-	m = pattern.search(sequenceADN, positionRecherche)
-	while m != None:
-		listPosMotif.append(m.start(0))
-		m = pattern.search(sequenceADN, m.start(0)+1)
-	return listPosMotif
+    sequenceADN = sequenceADN.upper()
+    pattern = re.compile(motif)
+    listPosMotif = []
+    m = pattern.search(sequenceADN, positionRecherche)
+    while m != None:
+        listPosMotif.append(m.start(0))
+        m = pattern.search(sequenceADN, m.start(0)+1)
+    return listPosMotif
+
 
 def signature(sequenceADN, m):
-	sequenceADN = sequenceADN.lower()
-	dictSignature = {}	
-	if m in range(0,11):
-		for i in range(0, len(sequenceADN), 1):
-			dictSignature[sequenceADN[i:i+m]] = dictSignature.get(sequenceADN[i:i+m], 0) + 1
-		cleInvalide = [ j for j in dictSignature if len(j) != m ]
-		for k in cleInvalide:
-			del dictSignature[k]
-		return dictSignature
-	else:
-		return {"Taille signature superieur a 10":"Erreur"}
+    sequenceADN = sequenceADN.lower()
+    dictSignature = {}
+    if m in range(0,11):
+        for i in range(0, len(sequenceADN), 1):
+            dictSignature[sequenceADN[i:i+m]] = dictSignature.get(sequenceADN[i:i+m], 0) + 1
+        cleInvalide = [ j for j in dictSignature if len(j) != m ]
+        for k in cleInvalide:
+            del dictSignature[k]
+        return dictSignature
+    else:
+        return {"Taille signature superieur a 10":"Erreur"}
